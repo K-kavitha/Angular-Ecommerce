13f1df4962d9c71ca4ed56b21dcb972fe4128128
K-kavitha <pavunkavitha191@gmail.com>
2024-01-16 22:02:44 +0530

    commit message

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { AuthGuard } from './auth.guard';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { NgModel } from '@angular/forms';
+const routes: Routes = [
+  {
+  component:HomeComponent,
+  path:'',
+  },
+  {
+    component:SellerAuthComponent,
+    path:'seller-auth',
+  },
+  {
+    component:SellerHomeComponent,
+    path:'seller-home',
+    canActivate:[AuthGuard]
+  },{
+    component:SellerAddProductComponent,
+    path:'seller-add-product',
+    canActivate:[AuthGuard]
+  },{
+    component:SellerUpdateProductComponent,
+    path:'seller-update-product/:id',
+    canActivate:[AuthGuard]
+  }
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
